304cdc691d453a479ddd03811739f32cb03253ae
alerubira <100098255+alerubira@users.noreply.github.com>
2024-05-09 12:46:03 -0300

    armando recetario

--- conexxion.js
+++ conexxion.js
@@ -5,8 +5,8 @@ import serveStatic from 'serve-static';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import bodyParser from'body-parser';
-
-
+let profecional;
+let encabezado;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,29 +19,74 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar Express para servir archivos estáticos desde la carpeta 'public'
 app.use(serveStatic(path.join(__dirname, 'estatica')));
-
+app.use(express.urlencoded({ extended: true }))
 // Configurar Express para usar Pug como motor de plantillas y establecer la carpeta de vistas
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'estatica'));
 
 // Definir una ruta para renderizar una vista Pug
 app.get('/', (req, res) => {
-  let encabezado="Bienvenido al Ministerio de Salud";
+   encabezado="Bienvenido al Ministerio de Salud";
     res.render('vistaPrincipal',{encabezado});
   });
-app.post('/verificarProfecional',(req,res)=>{
+  app.post('/verificarProfecional', (req, res) => {
     // Obtener el ID del Profesional enviado desde el formulario
-      const idProfesional = req.body.idProfesional;
-      console.log(idProfesional);
-});
+    const idProfecional = req.body.idProfecional;
+    if (idProfecional==="123") {
+
+      profecional="Alejandro";
+      // Redirigir al usuario al endpoint '/recetas'
+    return res.redirect('/recetas');
+    }else{
+      let alerta=true;
+     return res.render('vistaPrincipal',{encabezado,alerta})
+    }
+   
+  });
+  
+  app.get('/recetas', (req, res) => {
+     encabezado = "Bienvenido al Recetario web Nacional";
+     //Nombre, Apellido, documento, profesión, especialidad, domicilio y matrícula del profesional
+    profecional={nombre:"Alejandro",apellido:"Rubira",dni:"26833093",profecion:"medico",especialidad:"cardiologia",domiclio:"Tilisarao",matriculaP:"123"};
+    
+    res.render('recetas', { encabezado,  profecional }); // Pasar el nombre a la vista
+  });
+  app.get('/medicos',(req,res)=>{
+    res.render('medicos',{encabezado,nombre: profecional});
+  });
+  app.get('/medicamentos',(req,res)=>{
+    res.render('medicamentos',{encabezado,nombre: profecional});
+  })
+  
 app.post('/verificarAdministrativo',(req,res)=>{
+      // Obtener el ID del Profesional enviado desde el formulario
+    const idProfecional = req.body.idAdministrativo;
+    if (idProfecional==="123") {
+      encabezado="Planilla de medicamentos";
+      profecional="Alejandro";
+      // Redirigir al usuario al endpoint '/recetas'
+    return res.redirect('/medicamentos');
+    }else{
+      let alerta=true;
+     return res.render('vistaPrincipal',{encabezado,alerta})
+    }
+   
+});
+app.post('/verificarAdministrativoR',(req,res)=>{
+  // Obtener el ID del Profesional enviado desde el formulario
+const idProfecional = req.body.idAdministrativoR;
+if (idProfecional==="123") {
+   encabezado="Planilla de profecionales";
+  profecional="Alejandro";
+  // Redirigir al usuario al endpoint 
+return res.redirect('/medicos');
+}else{
+  let alerta=true;
+ return res.render('vistaPrincipal',{encabezado,alerta})
+}
 
 });
-app.get('/recetas',(req,res)=>{
-  let encabezado="Bienvenido al Recetario web Nacional";
 
-    res.render('recetas',{encabezado})
-});
   // Iniciar el servidor
   app.listen(port, () => {
     console.log(`Servidor Express escuchando en el puerto ${port}`);
